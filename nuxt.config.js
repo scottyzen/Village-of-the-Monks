0d91e5b0ee66cd37f23233110fc922941c97c11f
Scott Kennedy <scottyzen@gmail.com>
2020-02-09 11:04:14 +0000

    Changed project name

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || "",
+    title: process.env.npm_package_name || "Graiguenamanagh | Village of the Monks",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
