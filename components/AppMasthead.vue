9d6c81174df84a8ab3460198e32088e6beb5b1b4
Scott Kennedy <scottyzen@gmail.com>
2020-03-14 14:47:12 +0000

    Made homepage responsive

--- components/AppMasthead.vue
+++ components/AppMasthead.vue
@@ -2,10 +2,15 @@
   <section class="masthead"></section>
 </template>
 
-
 <style lang="scss" scoped>
 section.masthead {
   width: 100%;
-  height: 100vh;
+  height: 85vh;
+}
+@media (min-width: 768px) {
+  section.masthead {
+    width: 100%;
+    height: 100vh;
+  }
 }
 </style>
