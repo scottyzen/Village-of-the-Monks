c3c4016cf0bcd78ac495b8d511084675862ce578
Scott Kennedy <scottyzen@gmail.com>
2020-03-25 22:52:13 +0000

    Removed reen placeholder

--- components/AppMasthead.vue
+++ components/AppMasthead.vue
@@ -1,29 +1,8 @@
 <template>
-  <section :class="{ lazymasthead: !fullyLoaded }" class="masthead"></section>
+  <section class="masthead"></section>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      fullyLoaded: false
-    };
-  },
-  mounted() {
-    setTimeout(() => {
-      this.fullyLoaded = true;
-    }, 0);
-  }
-};
-</script>
-
 <style lang="scss" scoped>
-.lazymasthead {
-  background: url("https://res.cloudinary.com/odriscolls/image/upload/q_auto,f_auto/v1584304562/brush-strokes2.png")
-      center bottom no-repeat,
-    url("../assets/images/lazybg.jpg") repeat !important;
-  background-size: 10px, cover;
-}
 section.masthead {
   width: 100%;
   height: 75vh;
