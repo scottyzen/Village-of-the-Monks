07a197a28d96f429f3d4c21266ddfe5f53587c36
Scott Kennedy <scottyzen@gmail.com>
2020-08-06 21:54:38 +0100

    Revert bact to ssr

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ let dynamicRoutes = async () => {
 
 export default {
   mode: "universal",
-  target: "static",
+  
   // Headers of the page
   head: {
     title: "Graiguenamanagh | Village of the Monks",
