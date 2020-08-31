7970c702749ea9ba2dce79152814a3ae2b2c0ea5
Scott Kennedy <scottyzen@gmail.com>
2020-08-31 09:56:00 +0100

    installed dotenv

--- functions/cloundinaryGallery.js
+++ functions/cloundinaryGallery.js
@@ -14,27 +14,29 @@ exports.handler = function(event, context, callback) {
   console.log('Here comes te magic');
 
   //-- We only care to do anything if this is our POST request.
-  if(event.httpMethod !== 'POST' || !event.body) {
-    callback(null, {
-      statusCode,
-      headers,
-      body: ''
-    });
-  }
+//   if(event.httpMethod !== 'POST' || !event.body) {
+//     callback(null, {
+//       statusCode,
+//       headers,
+//       body: ''
+//     });
+//   }
 
   //-- Parse the body contents into an object.
   const data = JSON.parse(event.body);
   
   // -- Make sure we have all required data. Otherwise, escape.
-  if( !data.tag ) {
-    console.error('Required information is missing.');
-    callback(null, {
-      statusCode,
-      headers,
-      body: JSON.stringify({status: 'missing-information'})
-    });
-    return;
-  }
+//   if( !data.tag ) {
+//     console.error('Required information is missing.');
+//     callback(null, {
+//       statusCode,
+//       headers,
+//       body: JSON.stringify({status: 'missing-information'})
+//     });
+//     return;
+//   }
+
+console.log('AXIOS URL: ', `https://${CLOUDINARY_API_KEY}:${CLOUDINARY_API_SECRET}@api.cloudinary.com/v1_1/odriscolls/resources/image/tags/${data.tag}`);
 
   axios.get(`https://${CLOUDINARY_API_KEY}:${CLOUDINARY_API_SECRET}@api.cloudinary.com/v1_1/odriscolls/resources/image/tags/${data.tag}`).then(res => {
       callback(null, {
